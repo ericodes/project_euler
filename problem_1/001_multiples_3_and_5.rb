81a49762850165bd4392730e534bbff9e89bf9ac
ericodes <erica.cheung@flatironschool.com>
2014-09-11 14:46:07 -0400

    Solved problem 1

---
+++ problem_1/001_multiples_3_and_5.rb
@@ -0,0 +1,16 @@
+# https://projecteuler.net/problem=1
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def solver(limit)
+  i = 1
+  sum = 0
+  Array(1...limit).each do |i|
+    sum += i if (i % 3 == 0) || (i % 5 == 0)
+  end
+  sum
+end
+
+puts solver(1000)
